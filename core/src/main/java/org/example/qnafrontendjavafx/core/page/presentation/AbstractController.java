542e4e84941690e73da2399fcc44c45a249133f0
glass <silver20190204@gmail.com>
2024-12-30 14:03:02 +0900

    Refactor AbstractController

--- core/src/main/java/org/example/qnafrontendjavafx/core/page/presentation/AbstractController.java
+++ core/src/main/java/org/example/qnafrontendjavafx/core/page/presentation/AbstractController.java
@@ -2,10 +2,15 @@ package org.example.qnafrontendjavafx.core.page.presentation;
 
 import javafx.scene.layout.Pane;
 import org.example.qnafrontendjavafx.core.loader.IEventPublisher;
+import org.example.qnafrontendjavafx.core.page.application.AbstractService;
 
 public abstract class AbstractController {
+    protected final AbstractService service;
     protected IEventPublisher publisher;
 
+    AbstractController(AbstractService service) {
+        this.service = service;
+    }
     public final void setEventPublisher(IEventPublisher publisher) {
         this.publisher = publisher;
     }
