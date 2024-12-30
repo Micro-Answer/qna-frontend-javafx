5b6e531d5288fc3fceda50ba2f7f21f6c4fc65b1
glass <silver20190204@gmail.com>
2024-12-30 13:55:43 +0900

    Design application layer

---
+++ core/src/main/java/org/example/qnafrontendjavafx/core/page/application/AbstractService.java
@@ -0,0 +1,11 @@
+package org.example.qnafrontendjavafx.core.page.application;
+
+import org.example.qnafrontendjavafx.core.page.shared.IUser;
+
+public abstract class AbstractService {
+    protected IUser user;
+
+    public AbstractService(IUser user) {
+        this.user = user;
+    }
+}
---
+++ core/src/main/java/org/example/qnafrontendjavafx/core/page/shared/IUser.java
@@ -0,0 +1,4 @@
+package org.example.qnafrontendjavafx.core.page.shared;
+
+public interface IUser {
+}
